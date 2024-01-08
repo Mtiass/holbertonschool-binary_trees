192eadf31a731b3ea3fd34f0e34d9a7f3dfe1942
jorge871 <jg303114@gmail.com>
2024-01-08 18:07:41 -0300

    add task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,4 +7,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t con = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent != NULL)
+	{
+		con = binary_tree_depth(tree->parent);
+		con++;
+	}
+	return (con);
 }
