751254345ba35ee02536121a0a97f211c3e83701
jorge871 <jg303114@gmail.com>
2024-01-09 16:48:21 -0300

    fixed task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,6 +29,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	}
 	return (0);
 }
+}
 /**
  * binary_tree_height -  function that measures the height of a binary tree.
  * @tree: is a pointer to the root node of the tree to measure the height.
