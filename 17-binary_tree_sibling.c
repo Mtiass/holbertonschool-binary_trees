d122e893c3ce77b54b0fd22c8d670f8567d0f832
jorge871 <jg303114@gmail.com>
2024-01-09 17:04:51 -0300

    add task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,4 +7,16 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->left != NULL && node->parent->right != NULL)
+	{
+		if (node == node->parent->left)
+			return (node->parent->right);
+		if (node == node->parent->right)
+			return (node->parent->left);
+	}
+	return (NULL);
 }
