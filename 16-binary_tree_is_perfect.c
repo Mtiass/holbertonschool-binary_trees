b9c6f9edc5752e9d5bf69defcf2f8029eb63f712
jorge871 <jg303114@gmail.com>
2024-01-09 16:45:28 -0300

    fixed task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,14 +11,23 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if (left == right)
-		return (1);
-	if (binary_tree_is_perfect(tree->left)
-	&& binary_tree_is_perfect(tree->right))
-	    return (0);
-	return (!binary_tree_is_perfect(tree->left));
+	{
+		if (binary_tree_is_perfect(tree->left))
+		{
+			if (binary_tree_is_perfect(tree->right))
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
 /**
  * binary_tree_height -  function that measures the height of a binary tree.
